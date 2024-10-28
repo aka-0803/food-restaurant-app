23d0fca6562763bfae2d6107496fabb33f58bb17
Akash <akashbahadursingh5@gmail.com>
2024-10-28 22:16:24 +0530

    made db connection using mongodb

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const connectDB = require("./config/db");
+
+//dotenv configuration 
+dotenv.config();
+
+//DB connection
+connectDB();
+
+const app = express();
+
+//PORT
+const PORT = process.env.PORT || 5000;
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .send("<h1>Welcome to Amma Kitchen </h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
